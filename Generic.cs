456e673ac6f8043845d976e751773c732df9fc7a
Tadipartisahithi <003CHT744@IBM-PF3CYCFF>
2022-03-06 10:56:46 +0530

    Add project files.

---
+++ Generic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Assignament1
+{
+    internal class Generic
+    {
+        static void Swap<T>(ref T a,ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a = 50, b = 38;
+            Console.WriteLine("Before swap:{0},{1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine("After swap:{0},{1}", a, b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Assignament1
+{
+    class Rectangle
+    {
+        public double Width;
+        public double length;
+
+        public double GetArea()
+        {
+            return length * Width;
+        }
+        public void display()
+        {
+            Console.WriteLine("Width:{0}", Width);
+            Console.WriteLine("Length:{0}", length);
+            Console.WriteLine("Area:{0}", GetArea());
+        }
+    }
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle()    ;
+            rect.Width = 8.5;
+            rect.length = 6;
+            rect.display();
+            Console.ReadLine();
+        }
+    }
+}
